4c7e382bcac94d43b49496646539bb7f77b32fd5
Drielixxiii <drieli.freitas286@gmail.com>
2023-03-31 11:36:47 -0300

    commit

--- script.js
+++ script.js
@@ -1,20 +1,99 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-const tabContent = document.querySelectorAll('.js-tabcontent section')
+// FOTOS DOS ANIMAIS & DESCRIÇÃO
+function initTabNav() {
+ const tabMenu = document.querySelectorAll('.js-tabmenu li')
+ const tabContent = document.querySelectorAll('.js-tabcontent section')
 
-if (tabMenu.length && tabContent.length) {
- tabContent[0].classList.add('ativo')
+ if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add('ativo')
 
- function activeTab(index) {
-  tabContent.forEach((section) => {
-   section.classList.remove('ativo')
+  function activeTab(index) {
+   tabContent.forEach((section) => {
+    section.classList.remove('ativo')
+   })
+
+   tabContent[index].classList.add('ativo')
+  }
+
+  tabMenu.forEach((itemMenu, index) => {
+   itemMenu.addEventListener('click', () => {
+    activeTab(index)
+   })
+  })
+ }
+}
+
+initTabNav()
+
+// ACORDEON DO FAQ
+function initAccordion() {
+ const accordionList = document.querySelectorAll('.js-accordion dt')
+ const activeClass = 'ativo'
+ if (accordionList.length) {
+  accordionList[0].classList.add(activeClass)
+  accordionList[0].nextElementSibling.classList.add(activeClass)
+
+  function activeAccordion() {
+   this.classList.toggle(activeClass)
+   this.nextElementSibling.classList.toggle(activeClass)
+  }
+
+  accordionList.forEach((item) => {
+   item.addEventListener('click', activeAccordion)
+  })
+ }
+}
+
+initAccordion()
+
+// SCROLL SUAVE PARA LINK INTERNO
+function initScrollSuave() {
+ const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+ function scrollToSection(evento) {
+  evento.preventDefault()
+
+  const href = evento.currentTarget.getAttribute('href')
+  const section = document.querySelector(href)
+  const topo = section.offsetTop
+
+  // section.scrollIntoView({
+  //  behavior: 'smooth',
+  //  block: 'start',
+  // })
+
+  // forma alternativa
+  window.scrollTo({
+   top: topo,
+   behavior: 'smooth',
   })
 
-  tabContent[index].classList.add('ativo')
  }
 
- tabMenu.forEach((itemMenu, index) => {
-  itemMenu.addEventListener('click', () => {
-   activeTab(index)
-  })
+ linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSection)
  })
 }
+initScrollSuave()
+
+// ANIMAÇÃO AO SCROLL
+function initAnimacao() {
+ const sections = document.querySelectorAll('.js-scroll')
+ if (sections.length) {
+  const windowMetade = window.innerHeight * 0.6
+
+  function animaScroll() {
+   sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top - windowMetade
+    if (sectionTop < 0) {
+     section.classList.add('ativo')
+    }
+   })
+  }
+  animaScroll()
+  window.addEventListener('scroll', animaScroll)
+ }
+}
+
+initAnimacao()
+
+//
